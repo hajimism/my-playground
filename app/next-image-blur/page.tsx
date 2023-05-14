0b45eb3fd9ca90713635915261236acc119e4f8e
hajimism <h_nakagawa@yumemi.co.jp>
2023-05-14 14:36:25 +0900

    chore: add title in /next-image-blur

--- app/next-image-blur/page.tsx
+++ app/next-image-blur/page.tsx
@@ -1,13 +1,26 @@
+import { H1 } from "@/components/ui/Typography";
+
 import { Image } from "./image";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center min-h-screen">
+    <>
+      <H1 className="my-8">
+        next/image with{" "}
+        <a
+          href="https://plaiceholder.co/"
+          target="_blank"
+          rel="noopener"
+          className="underline"
+        >
+          plaiceholder
+        </a>
+      </H1>
       {/* @ts-expect-error Async Server Component */}
       <Image
         src="https://opengraph.githubassets.com/3e2e0d38d0967422099b2487ecab99b5ee9b752afb81043623b2571c9a96e540/hajimism/my-playground"
         alt=""
       />
-    </div>
+    </>
   );
 }
