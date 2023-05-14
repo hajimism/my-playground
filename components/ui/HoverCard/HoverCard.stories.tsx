c9cf4e96abc0699be0952102416343a5dad3c593
hajimism <h_nakagawa@yumemi.co.jp>
2023-05-14 13:09:54 +0900

    feat: add hover card

---
+++ components/ui/HoverCard/HoverCard.stories.tsx
@@ -0,0 +1,25 @@
+import { StoryObj, Meta } from '@storybook/react'
+
+import RootLayout from "@/app/layout";
+
+import { HoverCard } from '.'
+
+const meta: Meta<typeof HoverCard> = {
+  title: 'components/HoverCard',
+  component: HoverCard,
+  decorators: [
+    (Story) => (
+      <RootLayout>
+        <Story />
+      </RootLayout>
+    ),
+  ],
+}
+
+export default meta
+
+type Story = StoryObj<typeof HoverCard>
+
+export const Primary: Story = {
+  args:{}
+}
---
+++ components/ui/HoverCard/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
+import { forwardRef, ElementRef, ComponentPropsWithoutRef } from "react";
+
+import { cn } from "@/lib/utils";
+
+const HoverCard = HoverCardPrimitive.Root;
+
+const HoverCardTrigger = HoverCardPrimitive.Trigger;
+
+const HoverCardContent = forwardRef<
+  ElementRef<typeof HoverCardPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
+>(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+  <HoverCardPrimitive.Content
+    ref={ref}
+    align={align}
+    sideOffset={sideOffset}
+    className={cn(
+      "z-50 w-64 rounded-md border border-sage-5/50 bg-sage-1 p-4 text-sage-11 shadow-md outline-none animate-in zoom-in-90",
+      className
+    )}
+    {...props}
+  />
+));
+HoverCardContent.displayName = HoverCardPrimitive.Content.displayName;
+
+export { HoverCard, HoverCardTrigger, HoverCardContent };
