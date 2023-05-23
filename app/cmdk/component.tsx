d38cc978c79b55266b23ae36998ad18888c331e9
hajimism <h_nakagawa@yumemi.co.jp>
2023-05-23 22:26:30 +0900

    feat: add links to command menu

--- app/cmdk/component.tsx
+++ app/cmdk/component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SunMedium, Moon } from "lucide-react";
+import { SunMedium, Moon, Link } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useTheme } from "next-themes";
 import React from "react";
@@ -14,6 +14,8 @@ import {
   CommandList,
   CommandSeparator,
 } from "@/components/ui/Command";
+
+import { LINKS } from "../links";
 
 export const CommandMenu = () => {
   const router = useRouter();
@@ -42,7 +44,19 @@ export const CommandMenu = () => {
       <CommandInput placeholder="Type a command or search..." />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
-
+        <CommandGroup heading="Links">
+          {LINKS.map((page) => (
+            <CommandItem
+              key={page.path.toString()}
+              onSelect={() => {
+                runCommand(() => router.push(page.path));
+              }}
+            >
+              <Link className="mr-2 h-4 w-4" />
+              {page.path.toString()}
+            </CommandItem>
+          ))}
+        </CommandGroup>
         <CommandSeparator />
         <CommandGroup heading="Theme">
           <CommandItem onSelect={() => runCommand(() => setTheme("light"))}>
