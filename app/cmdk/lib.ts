230861640b8306b70614a00d4e1177b1f5dc10ec
hajimism <h_nakagawa@yumemi.co.jp>
2023-05-31 14:02:53 +0900

    fix: remove not implemented cache()

--- app/cmdk/lib.ts
+++ app/cmdk/lib.ts
@@ -1,5 +1,3 @@
-import { cache } from "react";
-
 import { LINKS } from "../links";
 
-export const getLinks = cache(() => Promise.resolve({ links: LINKS }));
+export const getLinks = () => Promise.resolve({ links: LINKS });
