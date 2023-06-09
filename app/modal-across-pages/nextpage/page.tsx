44699412e0526eb55d3fbf331a15ecfaaed32c47
hajimism <h_nakagawa@yumemi.co.jp>
2023-06-09 09:59:32 +0900

    chore: fix route

--- app/modal-across-pages/nextpage/page.tsx
+++ app/modal-across-pages/nextpage/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <div>
       <H1 className="mb-4">案件詳細ページ</H1>
-      <Link href="/modal-across-pages/">前のページに戻る</Link>
+      <Link href="/modal-across-pages">前のページに戻る</Link>
       <Modal />
     </div>
   );
