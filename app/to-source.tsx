67a544f122319b96548ada1d7df8abe9505e4e86
hajimism <h_nakagawa@yumemi.co.jp>
2023-06-17 15:51:06 +0900

    feat: add ToSource component

---
+++ app/to-source.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Code2 } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+import { LINKS } from "./links";
+
+const PATH_TO_TREE_IN_REOO =
+  "https://github.com/hajimism/my-playground/tree/main/app";
+
+export const ToSource = () => {
+  const pathname = usePathname();
+  const pageLink = LINKS.find((link) => link.path === pathname);
+  if (!pageLink) return <></>;
+
+  const githubLink = `${PATH_TO_TREE_IN_REOO}${pageLink.path.toString()}`;
+
+  return (
+    <div className="flex h-16 w-16 items-center justify-center rounded-full bg-muted shadow-lg">
+      <a href={githubLink} target="_blank" rel="noopener">
+        <Code2 className="pb-px pl-px" />
+      </a>
+    </div>
+  );
+};
