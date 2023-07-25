720ded546a6c551849418f6b62e85a87435a2535
hajimism <h_nakagawa@yumemi.co.jp>
2023-07-26 07:11:32 +0900

    feat: UIの意図を伝えるための文言を追加

--- app/(playground)/profile-generator/_components/preview.tsx
+++ app/(playground)/profile-generator/_components/preview.tsx
@@ -10,27 +10,30 @@ import { Logo } from "./logo";
 
 export const Preview = () => {
   return (
-    <div className="my-8 border-2 border-dotted border-blue-12 p-1">
-      <div className="aspect-video rounded-xl bg-blue-8 bg-[url('/profile-generator/background.png')] bg-contain p-6">
-        <div className="flex h-full items-center justify-between gap-4 bg-white p-16">
-          <div className="flex w-1/2 flex-col gap-6">
-            <div className="flex w-full items-center justify-between gap-4">
-              <ImagePreview />
-              <div className="flex w-1/2 flex-col gap-4">
-                <NamePreview />
-                <OccupationPreview />
+    <>
+      <span className="bg-white text-sm text-blue-12">ｷﾘﾄﾘ線</span>
+      <div className="relative border-2 border-dotted border-blue-12 p-1">
+        <div className="aspect-video rounded-xl bg-blue-8 bg-[url('/profile-generator/background.png')] bg-contain p-6">
+          <div className="flex h-full items-center justify-between gap-4 bg-white p-16">
+            <div className="flex w-1/2 flex-col gap-6">
+              <div className="flex w-full items-center justify-between gap-4">
+                <ImagePreview />
+                <div className="flex w-1/2 flex-col gap-4">
+                  <NamePreview />
+                  <OccupationPreview />
+                </div>
               </div>
+              <TagPreview />
+              <CommentPreview />
             </div>
-            <TagPreview />
-            <CommentPreview />
-          </div>
 
-          <div className="flex h-full shrink-0 flex-col justify-between gap-2">
-            <SkillPreview />
-            <Logo />
+            <div className="flex h-full shrink-0 flex-col justify-between gap-2">
+              <SkillPreview />
+              <Logo />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
